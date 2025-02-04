d87876f06fe23af9809ec47ed78e70c6220aab67
Prabhnoor551 <noorsaini317@gmail.com>
2025-02-04 20:53:49 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -4,21 +4,21 @@ int main()
    int a,b;
    char sign;
 
-   scanf("%d %d %c",&a,&b,&sign);
+   scanf("%d %d %c",&a, &b, &sign);
 
    switch(sign)
    {       
         case '+':
-        printf("%d",a+b);
+        printf("%d\n",a+b);
         break;
         case '-':
-        printf("%d",a-b);
+        printf("%d\n",a-b);
         break;
         case '*':
-        printf("%d",a*b);
+        printf("%d\n",a*b);
         break;
         case '/':
-        printf("%d",a/b);
+        printf("%d\n",a/b);
 
    }
 
